960c53804f3793ae7974ddf682098369e3e43059
Himanshu Gupta <147416747+himanshugupta2004@users.noreply.github.com>
2025-06-01 14:03:10 +0530

    code11

---
+++ advlistopr.py
@@ -0,0 +1,15 @@
+#advance concepts in list
+
+Scorelist = [85,90,62,52,55,78]
+
+#sorting of list
+Scorelist.sort()
+print (Scorelist)
+
+#reverse the list
+Scorelist.reverse()
+print (Scorelist)
+
+#creating list using pythonic code
+List_created = [x**2 for x in range (1,11)]
+print(List_created)
